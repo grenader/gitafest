711cf91007a8393c93237b711c7894b8f2dbaa74
Igor Kanshyn <igor.kanshyn@gmail.com>
2019-02-24 16:12:04 -0500

    Revert "corrected gulp for version 4"
    This reverts commit 2048699e

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ var banner = ['/*!\n',
 ].join('');
 
 // Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function(done) {
+gulp.task('vendor', function() {
 
   // Bootstrap
   gulp.src([
@@ -42,15 +42,12 @@ gulp.task('vendor', function(done) {
       './node_modules/font-awesome/fonts/*'
     ])
     .pipe(gulp.dest('./vendor/fonts'))
-
+  
   // bootstrap-social
   gulp.src([
       './node_modules/bootstrap-social/bootstrap-social.css'
     ])
     .pipe(gulp.dest('./vendor/bootstrap-social'))
-
-    done();
-
 });
 
 // Compile SCSS
@@ -63,7 +60,7 @@ gulp.task('css:compile', function() {
 });
 
 // Minify CSS
-gulp.task('css:minify', gulp.series('css:compile'), function() {
+gulp.task('css:minify', ['css:compile'], function() {
   return gulp.src([
       './css/*.css',
       '!./css/*.min.css'
@@ -77,17 +74,10 @@ gulp.task('css:minify', gulp.series('css:compile'), function() {
 });
 
 // CSS
-gulp.task('css', gulp.series('css:minify'), function () {
-    console.log("css talk is finished");
-//    done();
-});
+gulp.task('css', ['css:compile', 'css:minify']);
 
 // Default task
-gulp.task('default', gulp.parallel('css', 'vendor'), function (done) {
-        console.log("default talk is finished");
-        done();
-    }
-);
+gulp.task('default', ['css', 'vendor']);
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
@@ -99,11 +89,7 @@ gulp.task('browserSync', function() {
 });
 
 // Dev task
-gulp.task('dev', gulp.series('css', 'browserSync'), function() {
-  gulp.watch('./scss/*.scss').on('change', function(path, stats) {
-      console.log("csss watch update: "+path);
-      gulp.series('css');
-  });
-
+gulp.task('dev', ['css', 'browserSync'], function() {
+  gulp.watch('./scss/*.scss', ['css']);
   gulp.watch('./*.html', browserSync.reload);
 });
